8a08233446942eb5078dc4741e56114232be8481
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-08 16:28:14 -0400

    Pages render properly

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Nav from "./Navigation";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h1>Brandon Alexander</h1>
+            <Nav />
+        </header>
+    );
+}
