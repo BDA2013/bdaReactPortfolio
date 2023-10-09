fe4aff24a2114096150f307c5682e1031a5a137b
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-09 08:24:47 -0400

    Added details of myself on About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,13 @@ export default function About() {
     return (
         <div>
             <h1> About Me </h1>
+            <p>
+                I am a full stack web developer with a background in education and a passion for learning. I have a Bachelor's degree in Information Technology
+                and a Certificate of Completion from Rutgers Coding Bootcamp in Full Stack Web Development. I am currently working as a Package Handler at UPS and
+                currently looking to transition into a career in web development. I am a hard worker, a quick learner, and a team player. I am proficient in HTML,
+                CSS, JavaScript, jQuery, Node.js, Express.js, MySQL, MongoDB, React.js, and more. I am also proficient in Microsoft Office and Google Docs. I am
+                currently working on a personal social media project using the MERN stack.
+            </p>
         </div>
     );
 }
