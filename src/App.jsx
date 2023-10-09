86114c11b0a9cc7ef86561904e8ce0e3c41aeebd
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-09 08:28:44 -0400

    Formatted Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import './App.css'
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import initFontAwesome from './utils/initFontAwesome'
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import initFontAwesome from "./utils/initFontAwesome";
 
 initFontAwesome();
 
@@ -10,12 +10,12 @@ function App() {
   return (
     <>
       <Header />
-      <main className='mx-3'>
+      <main className="mx-3">
         <Outlet />
       </main>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/utils/initFontAwesome.jsx
+++ src/utils/initFontAwesome.jsx
@@ -1,7 +1,7 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {fab, faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons";
+import { fab, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function initFontAwesome() {
-    library.add(fab, faLinkedin, faGithub);
+  library.add(fab, faLinkedin, faGithub);
 }
 export default initFontAwesome;
