39053405e1f2f03a8851f411c02312446ee19d8c
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-17 14:13:21 -0400

    Form fix attempt #1

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return <div>
       <h1> Contact </h1>
-      <form name="contact">
+      <form name="contact" onSubmit="submit">
         <input type="hidden" name="form-name" value="contact" />
         <div>
           <label>
