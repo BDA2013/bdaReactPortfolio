b21119310f35bdda3dd5926a58bd07f9508f96d1
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-17 14:18:00 -0400

    Form fix attempt #3

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return <div>
       <h1> Contact </h1>
-      <form name="contact" netlify>
+      <form name="contact" method="POST" netlify>
         <input type="hidden" name="form-name" value="contact" />
         <div>
           <label>
