b20d48f5ee0d9368da55a4dba89e3807dfc852ac
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-17 13:57:39 -0400

    Added form to index.html

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return <div>
       <h1> Contact </h1>
-      <form name="contact" method="POST" netlify>
+      <form name="contact" method="POST">
         <input type="hidden" name="form-name" value="contact" />
         <div>
           <label>
