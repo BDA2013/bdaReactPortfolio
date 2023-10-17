2226317d3e98b945f8aa53a5a2d16a3ce245ad4b
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-17 12:52:33 -0400

    Fixed netlify implemtation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,20 +2,30 @@ export default function Contact() {
   return (
     <div>
       <h1> Contact </h1>
-      <form name="contact" method="POST" netlify>
-      <p>
-        <label>Your Name: <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" required /></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message" required></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+      <form name="contact" method="post" netlify onSubmit="submit">
+        <input type="hidden" name="form-name" value="contact" />
+        <div>
+          <label>
+            Full Name:
+            <input type="text" name="full-name" />
+          </label>
+        </div>
+        <p>
+          <label>
+            Your Email:
+            <input type="email" name="email" required />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <textarea name="message" required></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
   );
 }
