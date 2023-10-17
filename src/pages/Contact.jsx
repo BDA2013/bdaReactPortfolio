31b5e304c02a12f923c9955a773e123ea61e888c
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-17 15:10:01 -0400

    Moved success link to index.html

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return <div>
       <h1> Contact </h1>
-      <form name="contact" method="POST" netlify action="/contact/success">
+      <form name="contact" method="POST" netlify>
         <input type="hidden" name="form-name" value="contact" />
         <div>
           <label>
