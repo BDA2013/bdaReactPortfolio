38827fcad0145fdbe790b74bd1adf1c52e4a2f0a
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-22 19:55:54 -0400

    Reverted back to before

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,120 +1,40 @@
-import { useState } from "react";
-
-import { validateEmail } from "../utils/helpers";
-
 export default function Contact() {
-  const [formState, setFormState] = useState({
-    first_name: "",
-    last_name: "",
-    company: "",
-    email: "",
-    message: "",
-  });
-
-  const [errorMessage, setErrorMessage] = useState("");
-  const { first_name, last_name, company, email, message } = formState;
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      const formData = formState;
-
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(formData).toString(),
-      })
-        .then(() => alert("Thank you for your submission"))
-        .catch((error) => alert(error));
-    }
-    document
-      .querySelector("form")
-      .addEventListener("submit", handleSubmit);
-  };
-
-  const handleChange = (e) => {
-    if (e.target.name === "email") {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage("Your email is invalid.");
-      } else {
-        setErrorMessage("");
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage("");
-      }
-    }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log("Handle Form", formState);
-    }
-  };
-
-  return (
-    <section>
+  return <div>
       <h1> Contact </h1>
-      <form
-        name="contact"
-        method="POST"
-        onSubmit={handleSubmit}
-        data-netlify="true"
-      >
+      <form name="contact" method="POST" data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
         <div>
-          <label htmlFor="first_name">First Name:</label>
-          <input
-            type="text"
-            name="first_name"
-            defaultValue={first_name}
-            onBlur={handleChange}
-          />
+          <label>
+            First Name:
+            <input type="text" name="first-name" required />
+          </label>
         </div>
         <div>
-          <label htmlFor="last_name">Last Name:</label>
-          <input
-            type="text"
-            name="last_name"
-            defaultValue={last_name}
-            onBlur={handleChange}
-          />
+          <label>
+            Last Name:
+            <input type="text" name="last-name" required />
+          </label>
         </div>
         <div>
-          <label htmlFor="company">Company:</label>
-          <input
-            type="text"
-            name="company"
-            defaultValue={company}
-            onBlur={handleChange}
-          />
+          <label>
+            Your Company:
+            <input type="text" name="company" />
+          </label>
         </div>
         <div>
-          <label htmlFor="email">Email address:</label>
-          <input
-            type="email"
-            name="email"
-            defaultValue={email}
-            onBlur={handleChange}
-          />
+        <label>Your Email: 
+          <input type="email" name="email" required />
+          </label>
         </div>
         <div>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            name="message"
-            rows="5"
-            defaultValue={message}
-            onBlur={handleChange}
-          />
+          <label>
+            Message:
+            <textarea name="message" required></textarea>
+          </label>
         </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        <button type="submit">Submit</button>
+        <div>
+          <button type="submit">Send</button>
+        </div>
       </form>
-    </section>
-  );
+    </div>
 }
