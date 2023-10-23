d25628f9570ba95ed3af28056880a30f3739781e
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-22 20:09:41 -0400

    Added in the handleChange for alerts

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,40 +1,104 @@
+import { useState } from "react";
+import { validateEmail } from "../utils/helpers";
+
 export default function Contact() {
-  return <div>
+  const [formState, setFormState] = useState({
+    firstName: "",
+    lastName: "",
+    company: "",
+    email: "",
+    message: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const { firstName, lastName, company, email, message } = formState;
+
+  const handleChange = (e) => {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+  
+
+
+  return <section>
       <h1> Contact </h1>
       <form name="contact" method="POST" data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
         <div>
-          <label>
+          <label htmlFor="first-name">
             First Name:
-            <input type="text" name="first-name" required />
           </label>
+          <input
+            type="text"
+            name="first-name"
+            defaultValue={firstName}
+            onBlur={handleChange}
+          />
         </div>
         <div>
-          <label>
+          <label htmlFor="last-name">
             Last Name:
-            <input type="text" name="last-name" required />
           </label>
+          <input
+            type="text"
+            name="last-name"
+            defaultValue={lastName}
+            onBlur={handleChange}
+          />
         </div>
         <div>
-          <label>
+          <label htmlFor="company">
             Your Company:
-            <input type="text" name="company" />
           </label>
+          <input
+            type="text"
+            name="company"
+            defaultValue={company}
+            onBlur={handleChange}
+          />
         </div>
         <div>
-        <label>Your Email: 
-          <input type="email" name="email" required />
-          </label>
+        <label htmlFor="email">Your Email:</label>
+        <input
+          type="email"
+          name="email"
+          defaultValue={email}
+          onBlur={handleChange}
+        />
         </div>
         <div>
-          <label>
-            Message:
-            <textarea name="message" required></textarea>
-          </label>
+          <label htmlFor="message">Message:</label>
+            <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onBlur={handleChange}
+          />
         </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
         <div>
           <button type="submit">Send</button>
         </div>
       </form>
-    </div>
+    </section>
 }
