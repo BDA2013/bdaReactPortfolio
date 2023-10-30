3e481241c3e05ea425768bbe368f3056a24a22d3
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-10-30 13:06:43 -0400

    Added descriptions to each projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,6 +7,7 @@ const projectList = [
     {
         id: 1,
         title: 'RiseUp',
+        description: 'RiseUp is a web application that allows users to find the times when the sunrise and sunset occurs.' ,
         link: 'https://bda2013.github.io/riseUp/',
         image: RiseUp,
         github: 'https://github.com/BDA2013/riseUp'
@@ -14,6 +15,7 @@ const projectList = [
     {
         id: 2,
         title: 'PlayCare',
+        description: 'PlayCare is a web application that allows parents to add and enroll their children in a daycare.' ,
         link: 'https://mysterious-anchorage-56643-889ec0494a37.herokuapp.com',
         image: PlayCare,
         github: 'https://github.com/USOYJ/playcare'
@@ -21,6 +23,7 @@ const projectList = [
     {
         id: 3,
         title: 'GameChip',
+        description: 'GameChip is a web application that allows users to find and post questions, or opinions about their favorite games.' ,
         link: 'https://radiant-everglades-30632-b3c22f102a99.herokuapp.com/',
         image: GameChip,
         github: 'https://github.com/SethMunoz339/gamerFAQ'
@@ -39,6 +42,7 @@ export default function Project() {
                                 <img src={project.image} className="card-img-top" alt={project.title} />
                                 <div className="card-body">
                                     <h5 className="card-title">{project.title}</h5>
+                                    <p className="card-text">{project.description}</p>
                                     <a href={project.link} className="btn btn-primary">Deployed</a>
                                     &nbsp;                                    
                                     <a href={project.github} className="btn btn-primary">Github</a>
